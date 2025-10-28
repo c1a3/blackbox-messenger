c147c73097d66c13da4bbb7d5ffe8ae78aaed203
c1a3 <154661452+c1a3@users.noreply.github.com>
2025-10-28 18:34:12 +0000

    revert

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,6 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-/*
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -40,7 +39,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-*/
 
 server.listen(PORT, async () => { // Make listener async
   console.log("Server is running on PORT:" + PORT);
